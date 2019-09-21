d3ee05bf61baffda6e630522bc8a7aa0f445e295
ChriisH <hoffmannchris@outlook.fr>
2019-09-21 14:54:52 +0200

    added http server for later, fixed some issues

--- H1Z1 LoginServer/Main.cpp
+++ H1Z1 LoginServer/Main.cpp
@@ -1,10 +1,31 @@
+#define WIN32_LEAN_AND_MEAN 
+#include "httplib.h"
+
 #include <Windows.h>
 #include <iostream>
 #include <memory>
 #include "Header.hpp"
 
+
 const std::string g_ip = "127.0.0.1";
 const int g_port = 20042;
+const int g_portapi = 8080;
+
+DWORD WINAPI LoginServerAPI(LPVOID arg)
+{
+	httplib::Server svr;
+
+	svr.Get("/hi", [](const httplib::Request & req, httplib::Response & res) {
+		res.set_content("Hello World!", "text/plain");
+	});
+
+	svr.Get("/stop", [&](const httplib::Request & req, httplib::Response & res) {
+		svr.stop();
+	});
+
+	svr.listen(g_ip.c_str(), g_portapi);
+	return 0;
+}
 
 int main()
 {
@@ -12,6 +33,7 @@ int main()
 	SetConsoleTitleA("H1Z1 LoginServer");
 
 	auto m_server = std::make_shared< c_udp_server >(g_ip, g_port);
+	HANDLE th = CreateThread(NULL, 0, LoginServerAPI, 0, 0, 0);
 
 	if (!m_server->setup())
 	{
